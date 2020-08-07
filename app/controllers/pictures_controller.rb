b61b1db4221094e9cd4522939e1a4d3f7250d1fc
penpen09 <ar-el.mbl.10@outlook.jp>
2020-08-07 20:07:47 +0900

    association and username_display

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -26,13 +26,12 @@ class PicturesController < ApplicationController
   end
 
   def confirm
-    @picture = Picture.new(picture_params)
-    @picture.id = params[:id]
+    @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
   end
 
   def create
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     if params[:back]
       render :new
     else
