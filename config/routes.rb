bf4673171ba5eb98874ce5403951c663264b7b7a
penpen09 <ar-el.mbl.10@outlook.jp>
2020-08-07 16:34:45 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
   resources :pictures
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
